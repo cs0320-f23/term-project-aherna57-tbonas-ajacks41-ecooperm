a8bb45319121e66819adb499633d5338b2f33115
ethan-cooperman <ethan_cooperman@brown.edu>
2023-11-30 00:30:05 -0500

    feat: prisma schemas for restaurants

--- backend/db.ts
+++ backend/db.ts
@@ -1,17 +1,14 @@
-// TODO: Update with .env file
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import { env } from "./env";
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-// import { env } from "~/env";
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
 
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: PrismaClient | undefined;
-// };
-
-// export const db =
-//   globalForPrisma.prisma ??
-//   new PrismaClient({
-//     log:
-//       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-//   });
-
-// if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
