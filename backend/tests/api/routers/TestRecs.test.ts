fad10ff5418b67d5f15d1734cb097dc6fe9de6ea
ethan-cooperman <ethan_cooperman@brown.edu>
2023-12-11 22:23:30 -0500

    feat: broken unit tests and seeding function

--- backend/tests/api/routers/TestRecs.test.ts
+++ backend/tests/api/routers/TestRecs.test.ts
@@ -1,6 +1,15 @@
-import { prisma } from "../../../db";
+import { PrismaClient } from "@prisma/client";
+import { appRouter } from "~/api/root";
 import assert from "assert";
 
+const prisma = new PrismaClient();
 test("add a new function to TestCommandToFunction", () => {
   console.log("hello world");
+});
+
+test("get boba shops from database", async () => {
+  const bobaShops = await appRouter.restaurants.getAll({
+    prisma: prisma,
+    userId: null,
+  });
 });
